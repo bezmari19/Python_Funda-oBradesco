dcdddd769c76f4d5cd0f274686772465b227f4b1
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-18 19:11:50 -0300

    Commit Feito

---
+++ HelloPyCharm.py
@@ -0,0 +1,2 @@
+print('Hello World, estou usando o PyCharm pela primeira vez!')
+#O teste foi feita pela IDE usada pelo curso, porém como já acostumei com o VScode decidi passar cada exercício feito para ele!
