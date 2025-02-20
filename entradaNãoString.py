9f52f69a60666959582947fe5515fc550b60018a
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-20 20:02:53 -0300

    commit feito

---
+++ entradaNãoString.py
@@ -0,0 +1,11 @@
+codigo = int(input())
+nome = input()
+salario = float(input())
+ativo = True
+#Usando as entradas input para receber os dados propostos 
+#Usando o mesmo bloco de códigos de uma das propostas anteriores
+
+print('Código: %d' % codigo) # %d para valores inteiros
+print('Nome: %s' % nome) # %s para cadeia de caracteres (string)
+print('Salário: %.2f' % salario) # %.2f para valor decimal decimais com duas casas
+print('Ativo: %r' % ativo) # %r para exibir True or False (Boolean)
