89c85a114aae2efec2603fc1c82a15c8f1ea0928
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-20 18:46:19 -0300

    commit feito

---
+++ tipoVariável.py
@@ -0,0 +1,7 @@
+codigo = 10 
+salario = 1500.00
+nome = 'José'
+#O intuito é imprir qual o tipo de váriavel o salario se encaixa, a resposta certa seria 'Float'
+tipo = type (salario)
+print(salario)
+print(tipo)
