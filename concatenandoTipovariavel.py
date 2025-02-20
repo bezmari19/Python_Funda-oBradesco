60ad9005c691b4eeb152ea4fcdb19604e35bdc36
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-20 18:56:03 -0300

    commit feito

---
+++ concatenandoTipovariavel.py
@@ -0,0 +1,8 @@
+codigo = 10
+salario = 1500.00
+nome = 'José'
+
+print('Código:', codigo, 'Nome:', nome, ' O salário atual é de:', salario)
+#Também pode ser concatenado com o valor de soma (+), para isso é preciso converter valores que não são str para str;
+#Após isso utilizamos o comando antes da váriavel ou seja str();
+#Assim ficaria da seguinte forma: print('Código:',+ str(codigo)+, 'Nome:', nome, ' O salário atual é de:',+ str(salario))
