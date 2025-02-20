687f1eb007c462895d86cc64705fdbb2cae01b70
bezmari19 <marina.bezerraf19@gmail.com>
2025-02-20 19:22:07 -0300

    commit feito

---
+++ exibirVariavel.py
@@ -0,0 +1,10 @@
+codigo = 105
+nome = 'Marina Bezerra'
+salario = 1650.00
+ativo = True
+#imprimir utilizando máscara de formatação
+print('Código: %d' % codigo) # %d para valores inteiros
+print('Nome: %s' % nome) # %s para cadeia de caracteres (string)
+print('Salário: %.2f' % salario) # %.2f para valor decimal decimais com duas casas
+print('Ativo: %r' % ativo) # %r para exibir True or False (Boolean)
+
